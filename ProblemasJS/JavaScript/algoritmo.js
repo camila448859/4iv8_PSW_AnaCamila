3e75db708f9e8d5c8cdad1d6e3b71c531c00e099
camila448859 <garcia.martinez.anacamila@gmail.com>
2022-03-16 11:10:13 -0600

    EjerciciosJS

--- ProblemasJS/JavaScript/algoritmo.js
+++ ProblemasJS/JavaScript/algoritmo.js
@@ -1,6 +1,59 @@
-function algoritmo(){
-    let cap_final, cap_inicial;
-    cap_inicial = parseFloat (document.formulario1.cap_inicial.value);
-    cap_final= cap_inicial*1.02;
-    document.formulario1.cap_final.value = cap_final;
+function validarc(e){
+    var datos = (document.all)?e.KeyCode : e.which
+
+    if(datos==8) return true;
+    var rango=/[0-9\d .]/
+
+    var probar = String.fromCharCode(datos);
+    return rango.test(probar);
+
+}
+function interes(){
+    var valor = document.ejercicio1.cantidad.value;
+
+    var resultado = parseFloat(valor);
+    var interes = resultado*0.020;
+    var total = resultado + interes;
+
+
+    document.ejercicio1.sueldoT.value = "$" + total
+}
+
+function borrardatos(){
+    document.ejercicio1.sueldoT.value = "";
+    document.ejercicio1.cantidad.value = "";
+}
+
+
+
+
+function calculaEdad(diaNacimiento, mesNacimiento, anioNacimiento)
+{
+  var diaNacimiento=document.getElementById("dia").value;
+  var mesNacimiento=document.getElementById("mes").value;
+  var anioNacimiento =document.getElementById("anio").value;
+  var salida=document.getElementById("salida");
+  
+        this.diaNacimiento=diaNacimiento;
+        this.mesNacimiento=mesNacimiento;
+        this.anioNacimiento=anioNacimiento;
+    
+        this.edad=function edad()
+        {
+           var calcula=0;
+           var objeto=new Date();
+           var dia=objeto.getDate();
+           var mes= objeto.getMonth()+1;
+           var hoy=objeto.getFullYear();
+
+                if(mes<this.mesNacimiento||(mes==this.mesNacimiento&&dia<this.dia))
+                {
+                  return calcula=parseInt(hoy-this.anioNacimiento)-1; 
+                }
+                else
+                {
+                   return calcula=parseInt(hoy-this.anioNacimiento);  
+                }
+          };
+    salida.innerHTML= "La edad actual de es:  "+this.edad()+".";
 }
